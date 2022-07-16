a96e93fd1e16b85fea085134bbec197711a60a8f
Thiago Alves <tpereiraalves2013@outlook.com>
2022-07-16 17:59:22 -0300

    Database

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -3,9 +3,13 @@ package com.devsuperior.dsmeta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DsmetaApplication {
 
+
+
+@SpringBootApplication 
+
+public class DsmetaApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DsmetaApplication.class, args);
 	}
